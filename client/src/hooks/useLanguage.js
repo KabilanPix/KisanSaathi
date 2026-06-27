fe78a3e1f542fba9f4d51c05f6ef91bb80d4b4b4
Kabilan <kabilan629@gmail.com>
2026-06-27 12:36:27 +0530

    UI: Change default language to English

--- client/src/hooks/useLanguage.js
+++ client/src/hooks/useLanguage.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 export function useLanguage() {
   const [language, setLanguage] = useState(() => {
-    return localStorage.getItem('kisan_language') || 'hi';
+    return localStorage.getItem('kisan_language') || 'en';
   });
 
   useEffect(() => {
